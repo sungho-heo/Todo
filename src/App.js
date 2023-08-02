6904fb1369b7b27ba4a59cddfe2e32c6d3b62656
Sungho <hurgj123kr@gmail.com>
2023-08-02 16:11:30 +0900

    react-router-dom retry update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useState } from "react";
 import Todo from "./Todo.js";
 import Login from "./Login.js";
@@ -6,11 +12,25 @@ function App() {
   const [name, setName] = useState("");
   return (
     <div>
-      {isLogin ? (
-        <Todo />
-      ) : (
-        <Login setIsLogin={setIsLogin} name={name} setName={setName} />
-      )}
+      {/* process.env.PUBLIC_URL > package.json homepage url connect default config.. */}
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route
+            path='/todo'
+            element={isLogin ? <Todo /> : <Navigate to='/' />}
+          ></Route>
+          <Route
+            path='/'
+            element={
+              isLogin ? (
+                <Navigate to='/todo' />
+              ) : (
+                <Login setIsLogin={setIsLogin} name={name} setName={setName} />
+              )
+            }
+          ></Route>
+        </Routes>
+      </Router>
       <footer>&copy; {new Date().getFullYear()} Todo All right reserved</footer>
     </div>
   );
